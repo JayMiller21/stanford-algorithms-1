2bb840db258251934de64d28ba0fb58e5f8563ad
Jennifer Miller <jennifer.a.miller2@gmail.com>
2015-08-14 19:08:58 -0400

    Update merge_sort(). Seems to work for all array lengths.

--- merge-sort.rb
+++ merge-sort.rb
@@ -1,12 +1,11 @@
-
 # Merge two sorted arrays
-
 def merge_sorted_arrays(a,b)
   
   n = a.length + b.length
   c = Array.new(n)
   i = 0
   j = 0
+
   for k in 0..n-1
     if a[i].nil?
       c[k] = b[j]
@@ -29,15 +28,25 @@ end
 
 
 # Merge Sort
-# Seems to work on all array lengths except 0 or 1
-
 def merge_sort(array)
+  
   n = array.length
+
   if n >= 2
     a = array[0..n/2-1]
     b = array[n/2..n-1]
     if a.length >= 2 then a = merge_sort(a) end
     if b.length >= 2 then b = merge_sort(b) end
+    merge_sorted_arrays(a,b)
+  else
+    array
   end
-  merge_sorted_arrays(a,b)
+
 end
+
+
+# Testing
+# x = (0..50).to_a.shuffle.take(30)
+# y = x.sort
+# z = merge_sort(x)
+# y == z
